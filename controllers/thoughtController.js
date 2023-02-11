62be52afef53b9b6f038ca1cfce86de9802ce836
Kayla Casale <kayla.casale@gmail.com>
2023-02-11 14:52:40 -0800

    added route and controllers to delete thought and update user by id

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -58,4 +58,24 @@ module.exports = {
             )
             .catch((err) => res.status(500).json(err));
     },
+    // delete a thought by id through '/api/thoughts/:thoughtId' 
+    deleteThought(req, res) {
+        Thought.findOneAndRemove({ _id: req.params.thoughtId })
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: "No thought found with this ID!" })
+                    : User.findOneAndUpdate(
+                        { thoughts: req.prams.thoughtId },
+                        { $pull: { thoughts: req.params.thoughtId } },
+                        { new: true }
+                    )
+            )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: "Thought deleted, but no user found" })
+                    : res.json({ message: "Thought successfully deleted" })
+            )
+            .catch((err) =>
+                res.status(500).json(err))
+    },
 }
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -6,14 +6,17 @@ const {
     getThoughts,
     getSingleThought,
     createThought,
-    updateThought
+    updateThought,
+    deleteThought
 } = require('../../controllers/thoughtController');
 
 // '/api/thoughts' to GET all thoughts and POST new thought
 router.route('/').get(getThoughts).post(createThought)
 
+// '/api/thoughts/:thoughtId' to GET one, PUT, and DELETE a thought by id
 router.route('/:thoughtId')
     .get(getSingleThought)
     .put(updateThought)
+    .delete(deleteThought)
 
 module.exports = router;
